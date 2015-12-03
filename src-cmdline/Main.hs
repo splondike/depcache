5f485a1a8cf4af0df5a8e3094dc83b18c29a99ce
Stefan Schneider-Kennedy <code@stefansk.name>
2015-12-03 22:25:49 +1100

    Remove leftover comment

--- src-cmdline/Main.hs
+++ src-cmdline/Main.hs
@@ -33,5 +33,4 @@ options = [target]
    where
       target = GO.Option "" ["targetDir"] (GO.ReqArg id "directory") "The repository to search for dependency description files"
 
--- TODO: Specify what each of the failure exit codes mean
 usage = GO.usageInfo "Usage: depcache --targetDir=<repo root>\nReturn code 0 indicates success, 1 general failure, 2 is failure to find dependency\n" options
